86eaa97b10433a4fad0c9310fa0d71d9e0cdf4f2
gitdagray <DaveOnEleven@gmail.com>
2021-03-08 12:31:05 -0600

    fixed typo in regex for serverless get_coords function

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -5,7 +5,7 @@ const { WEATHER_API_KEY } = process.env;
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { text, units } = params;
-  const regex = /^d+$/g;
+  const regex = /^\d+$/g;
   const flag = regex.test(text) ? "zip" : "q";
   const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`;
   const encodedUrl = encodeURI(url);
